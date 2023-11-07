e9d1d573a20e7b5612bc156f9d2027807e54c73b
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2023-11-07 18:20:40 -0500

    ,

--- scanning/network_scanner.py
+++ scanning/network_scanner.py
@@ -1,11 +1,12 @@
 import scapy.all as scapy
 import re
+import subprocess
 
 ip_add_range = re.compile('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$')
 ip_inputed = ''
 
 while True:
-    ip_inputed = input('IP address range: ')
+    ip_inputed = input('IP address range (ex: 192.168.1.0/24): ')
     if (ip_add_range.search(ip_inputed)):
         print(f'{ip_inputed} is valid')
         break
@@ -14,3 +15,6 @@ while True:
 print('---------------------------------------------------------------------------------------------------------')
 arp_result = scapy.arping(ip_inputed)
 print('---------------------------------------------------------------------------------------------------------')
+
+nmap_ip_input = input('Choose IP address to scan on nmap: ')
+print(subprocess.call(['nmap', nmap_ip_input]))
